0867cf412e67a64e00f31af91f92e226e55b4443
Paulo Henrique F. de Souza <phferreirasouza106@gmail.com>
2023-12-26 21:19:27 -0300

    feat: add native base provider intro Home screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
+
+import { theme } from '@themes/index'
+
+import { Home } from '@screens/Home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider theme={theme}>
+      <Home />
+    </NativeBaseProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from 'native-base'
+
+export function Home() {
+  return (
+    <Center flex={1} bg="muted.300">
+      <Text color="dark.100" fontSize="lg">
+        Hello World With Native Base
+      </Text>
+    </Center>
+  )
+}
